a018848cefe2b60edda1db2399f86d8f01c6832d
Javier <javierfh2003@gmail.com>
2023-03-13 16:45:03 +0100

    Añadido metodo sumar

--- Prueba/src/HelloWorld.java
+++ Prueba/src/HelloWorld.java
@@ -1,5 +1,10 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("¡Hola Mundo!");
+        System.out.println(sumar(2, 3));
+    }
+
+    public static int sumar(int x, int y){
+        return x + y;
     }
 }
